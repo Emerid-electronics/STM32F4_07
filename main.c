e0d64c5f4bfa7798426fffd2b01cee38dce3a707
Emerid-electronics <pawel.robert.mleczek@gmail.com>
2021-01-28 09:04:17 +0100

    Odbior dluzszych wiadomosci (10 znakow)

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ TIM_HandleTypeDef htim11;
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
-uint8_t received;
+uint8_t received[10];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -56,8 +56,8 @@ static void MX_GPIO_Init(void);
 static void MX_TIM11_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -93,12 +93,10 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM11_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim11);
   /* USER CODE END 2 */
-  HAL_UART_Receive_IT(&huart1,&received,1);
+  HAL_UART_Receive_IT(&huart1,&received,10);
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
@@ -243,41 +241,17 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	if (htim ->Instance == TIM11){
-		static uint16_t counter = 0;
-		uint8_t data[50];
-		uint16_t size = 0;
-
-		counter++;
-		size = sprintf(data, "Liczba wyslanych wiadomosci: %d.\n\r", counter);
-		HAL_UART_Transmit_IT(&huart1, data, size);
-		HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-	}
-}
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	uint8_t echo[50];
 	uint8_t size = 0;
 
-	switch (atoi(&received)){
-	case 0:
-		size = sprintf(echo, "STOP\r\n");
-		HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,RESET);
-		break;
-
-	case 1:
-		size = sprintf(echo, "START\r\n");
-		HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,SET);
-		break;
-
-	default:
-		size = sprintf(echo, "Odebrano nieznany znak: %c\r\n", received);
-		break;
-	}
-
+	size = sprintf(echo, "Odebrana wiadomosc: %s\r\n", received);
+	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
 	HAL_UART_Transmit_IT(&huart1, echo, size);
-	HAL_UART_Receive_IT(&huart1,&received,1);
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
+	HAL_UART_Receive_IT(&huart1,&received,10);
 }
 /* USER CODE END 4 */
 
